babd00e03696377af49b117e052b003ebef27f72
ruf <ruf@knowit.no>
2016-10-27 16:31:59 +0200

    Refactored and using buttons for init

--- src/Views.elm
+++ src/Views.elm
@@ -9,25 +9,41 @@ import Types exposing (..)
 
 view : Model -> Html Msg
 view model =
+    case model of
+        NotStarted ->
+            notStarted
+
+        Started seconds ->
+            viewTime seconds "green"
+
+        Finished ->
+            viewTime 0 "red"
+
+
+notStarted : Html Msg
+notStarted =
     let
-        bgColor =
-            if model > 0 then
-                "green"
-            else
-                "red"
+        timeButton minutes =
+            div [] [ button [ onClick (Start (minutes * 60)) ] [ text <| (toString minutes) ++ " minutes" ] ]
     in
-        div
-            [ style
-                [ ( "font-size", "50vh" )
-                , ( "text-align", "center" )
-                , ( "background-color", bgColor )
-                , ( "height", "100vh" )
-                ]
+        div []
+            (List.map timeButton [ 10, 15, 20, 25, 30, 35, 40, 45 ])
+
+
+viewTime : Seconds -> String -> Html Msg
+viewTime seconds bgColor =
+    div
+        [ style
+            [ ( "font-size", "50vh" )
+            , ( "text-align", "center" )
+            , ( "background-color", bgColor )
+            , ( "height", "100vh" )
             ]
-            [ text (formatSeconds model) ]
+        ]
+        [ text (formatSeconds seconds) ]
 
 
-formatSeconds : Int -> String
+formatSeconds : Seconds -> String
 formatSeconds s =
     let
         minutes =
